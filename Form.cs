599121e1fcea8487c71fe11ae8df7e19f6eaff85
Ythosa <vasus714@yandex.ru>
2020-07-05 18:11:42 +0300

    Add neightbours counter

--- Form.cs
+++ Form.cs
@@ -114,7 +114,23 @@ namespace GameOfLife
 
         private int CountNeightbours(int x, int y)
         {
-            return 0;
+            int count = 0;
+
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    int col = (x + i + cols) % cols;
+                    int row = (y + j + rows) % rows;
+
+                    bool isSelfChecking = col == x && row == y;
+                    bool hasLife = field[col, row];
+
+                    if (!isSelfChecking && hasLife) count++;
+                }
+            }
+
+            return count;
         }
     }
 }
